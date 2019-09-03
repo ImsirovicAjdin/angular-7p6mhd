7b5573fedc2a121cccd4c67d90dc074d72d9edf4
ImsirovicAjdin <ajdika@live.com>
2019-09-03 16:40:45 +0200

    Add all the starter code for all the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,21 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { CardsComponent } from './cards/cards.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent,
+    HeaderComponent,
+    JumbotronComponent,
+    CardsComponent,
+    FooterComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
